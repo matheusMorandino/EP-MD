946f48d00850958d5f75526871b431b6a7e1f3f3
matheus <matheus2060@gmail.com>
2022-01-01 19:39:39 -0300

    refat: refatora chamada do treino do modelo

--- modelo.py
+++ modelo.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 import warnings
+import pickle
 from sklearn.exceptions import ConvergenceWarning
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import LinearSVC
@@ -20,28 +21,9 @@ def create_model(x_train, y_train, pipeline, parameters):
 
     return grid_search
 
-if __name__ == "__main__":
-    # the training data folder must be passed as first argument
-    print(">>>Loading dataset")
-    dataset = pd.read_csv("lyrics_analysis.csv")
-    dataset['emotion'] = dataset["compound"].apply(lambda x: "pos" if x >= 0 else "neg" )
-
-    # split the dataset in training and test set:
+def train_model(dataset, pipeline, parameters):
     print(">>>Generanting test/train split")
-    docs_train, docs_test, y_train, y_test = train_test_split(
-        dataset['lyric'], dataset['emotion'], test_size=0.25, random_state=None)
-
-    # Pipeline for applying gridseach later
-    pipeline = Pipeline([
-        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
-        ('clf', LinearSVC(C=1000, max_iter=99999)),
-    ])
-
-    # Fit the pipeline on the training set using grid search for the parameters
-    print(">>>Applying grid seach")
-    parameters = {
-        'vect__ngram_range': [(1, 1), (1, 2)],
-    }
+    docs_train, docs_test, y_train, y_test = train_test_split(dataset['lyric'], dataset['emotion'], test_size=0.2, random_state=None)
 
     grid_search = create_model(docs_train, y_train, pipeline, parameters)
 
@@ -69,3 +51,39 @@ if __name__ == "__main__":
     print(">tn: " + str(tn.item()))
     print(">fn: " + str(fn.item()))
     print(">test acc : " + str(acc))
+
+    return grid_search
+
+
+if __name__ == "__main__":
+    # the training data folder must be passed as first argument
+    print(">>>Loading dataset")
+    dataset = pd.read_csv("other_scripts/lyrics_analysis_Clean.csv")
+    dataset['emotion'] = dataset["compound"].apply(lambda x: "pos" if x >= 0 else "neg" )
+
+    sys.stdout = open("test.txt", "w")
+
+    # Pipeline for applying gridseach later
+    pipeline = Pipeline([
+        ('vect', TfidfVectorizer(min_df=3, max_df=0.95)),
+        ('clf', LinearSVC(C=1000, max_iter=9999999, dual=True)),
+    ])
+
+    # Fit the pipeline on the training set using grid search for the parameters  
+    parameters = {
+        'vect__ngram_range': [(1, 1), (1, 2), (2, 2)],
+        'vect__use_idf': (True, False),
+        'vect__norm': ('l1', 'l2'),
+        'vect__sublinear_tf': (True, False),
+        'vect__smooth_idf': (True, False),
+        'clf__loss': ('hinge', 'squared_hinge'),
+        'clf__dual': (True, False),
+        'clf__class_weight': (True, False),
+    }
+
+    print(">>>Applying grid seach")
+    model = train_model(dataset, pipeline, parameters)
+    with open("modelo.pkl", 'wb') as file:
+        pickle.dump(model, file)
+
+    sys.stdout.close()
